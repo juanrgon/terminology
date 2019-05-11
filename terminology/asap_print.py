c582da087f1d427e4de8a986c810936a482fe5dd
Juan Gonzalez <jrg2156@gmail.com>
2019-05-11 19:46:35 -0400

    Fix asap_print for python 2

--- terminology/asap_print.py
+++ terminology/asap_print.py
@@ -1,8 +1,16 @@
+from __future__ import print_function
+import sys
+
+
 def asap_print(*args, **kwargs):
     """
     Print to terminal immediately.
 
     Equivalent to calling print with `flush=True`.
     """
-    kwargs["flush"] = True
-    print(*args, **kwargs)
+    if sys.version_info[:2] < (3, 3):
+        print(*args, **kwargs)
+        sys.stdout.flush()
+    else:
+        kwargs["flush"] = True
+        print(*args, **kwargs)
