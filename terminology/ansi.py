18f1641e7fa10ffe3e5ea88b494a15c246a2b4e2
Juan Gonzalez <jrg2156@gmail.com>
2020-04-08 14:23:45 -0400

    Add typing

--- terminology/ansi.py
+++ terminology/ansi.py
@@ -16,146 +16,146 @@ def disable():
         NO_COLOR = original
 
 
-def in_black(text):
+def in_black(text) -> "StyledText":
     return _change_text_color(text, AnsiCode.BLACK_TEXT)
 
 
-def in_blue(text):
+def in_blue(text) -> "StyledText":
     return _change_text_color(text, AnsiCode.BLUE_TEXT)
 
 
-def in_bold(text):
+def in_bold(text) -> "StyledText":
     non_bold = _remove_bold(text)
     return _apply_ansi_code(AnsiCode.BOLD, non_bold)
 
 
-def in_cyan(text):
+def in_cyan(text) -> "StyledText":
     return _change_text_color(text, AnsiCode.CYAN_TEXT)
 
 
-def in_green(text):
+def in_green(text) -> "StyledText":
     return _change_text_color(text, AnsiCode.GREEN_TEXT)
 
 
-def in_magenta(text):
+def in_magenta(text) -> "StyledText":
     return _change_text_color(text, AnsiCode.MAGENTA_TEXT)
 
 
-def in_red(text):
+def in_red(text) -> "StyledText":
     return _change_text_color(text, AnsiCode.RED_TEXT)
 
 
-def in_white(text):
+def in_white(text) -> "StyledText":
     return _change_text_color(text, AnsiCode.WHITE_TEXT)
 
 
-def in_yellow(text):
+def in_yellow(text) -> "StyledText":
     return _change_text_color(text, AnsiCode.YELLOW_TEXT)
 
 
-def on_black(text):
+def on_black(text) -> "StyledText":
     return _change_background_color(text, AnsiCode.BLACK_BACKGROUND)
 
 
-def on_blue(text):
+def on_blue(text) -> "StyledText":
     return _change_background_color(text, AnsiCode.BLUE_BACKGROUND)
 
 
-def on_cyan(text):
+def on_cyan(text) -> "StyledText":
     return _change_background_color(text, AnsiCode.CYAN_BACKGROUND)
 
 
-def on_green(text):
+def on_green(text) -> "StyledText":
     return _change_background_color(text, AnsiCode.GREEN_BACKGROUND)
 
 
-def on_magenta(text):
+def on_magenta(text) -> "StyledText":
     return _change_background_color(text, AnsiCode.MAGENTA_BACKGROUND)
 
 
-def on_red(text):
+def on_red(text) -> "StyledText":
     return _change_background_color(text, AnsiCode.RED_BACKGROUND)
 
 
-def on_white(text):
+def on_white(text) -> "StyledText":
     return _change_background_color(text, AnsiCode.WHITE_BACKGROUND)
 
 
-def on_yellow(text):
+def on_yellow(text) -> "StyledText":
     return _change_background_color(text, AnsiCode.YELLOW_BACKGROUND)
 
 
-def underlined(text):
+def underlined(text) -> "StyledText":
     non_underlined = _remove_underline(text)
     return _apply_ansi_code(AnsiCode.UNDERLINE, non_underlined)
 
 
-def visual_len(text):
+def visual_len(text) -> int:
     """The apparent visual length of this string in a terminal."""
     return len(text) if NO_COLOR else len(_remove_regex("\033\\[[0-9]*m", text))
 
 
 class StyledText(str):
-    def in_black(self):
+    def in_black(self) -> "StyledText":
         return in_black(self)
 
-    def in_blue(self):
+    def in_blue(self) -> "StyledText":
         return in_blue(self)
 
-    def in_bold(self):
+    def in_bold(self) -> "StyledText":
         return in_bold(self)
 
-    def in_cyan(self):
+    def in_cyan(self) -> "StyledText":
         return in_cyan(self)
 
-    def in_green(self):
+    def in_green(self) -> "StyledText":
         return in_green(self)
 
-    def in_magenta(self):
+    def in_magenta(self) -> "StyledText":
         return in_magenta(self)
 
-    def in_red(self):
+    def in_red(self) -> "StyledText":
         return in_red(self)
 
-    def in_white(self):
+    def in_white(self) -> "StyledText":
         return in_white(self)
 
-    def in_yellow(self):
+    def in_yellow(self) -> "StyledText":
         return in_yellow(self)
 
-    def on_black(self):
+    def on_black(self) -> "StyledText":
         return on_black(self)
 
-    def on_blue(self):
+    def on_blue(self) -> "StyledText":
         return on_blue(self)
 
-    def on_cyan(self):
+    def on_cyan(self) -> "StyledText":
         return on_cyan(self)
 
-    def on_green(self):
+    def on_green(self) -> "StyledText":
         return on_green(self)
 
-    def on_magenta(self):
+    def on_magenta(self) -> "StyledText":
         return on_magenta(self)
 
-    def on_red(self):
+    def on_red(self) -> "StyledText":
         return on_red(self)
 
-    def on_white(self):
+    def on_white(self) -> "StyledText":
         return on_white(self)
 
-    def on_yellow(self):
+    def on_yellow(self) -> "StyledText":
         return on_yellow(self)
 
-    def underlined(self):
+    def underlined(self) -> "StyledText":
         return underlined(self)
 
-    def visual_len(self):
+    def visual_len(self) -> int:
         """The apparent visual length of this string in a terminal."""
         return visual_len(self)
 
 
-def _apply_ansi_code(ansi_code, text):
+def _apply_ansi_code(ansi_code, text) -> StyledText:
     if NO_COLOR:
         return StyledText(text)
 
@@ -166,34 +166,34 @@ def _apply_ansi_code(ansi_code, text):
     return StyledText(start + text + end)
 
 
-def _change_text_color(text, color_code):
+def _change_text_color(text, color_code) -> StyledText:
     """Change the color of text to the given color code."""
     uncolored_fg = _remove_text_colors(text)
     return _apply_ansi_code(color_code, uncolored_fg)
 
 
-def _change_background_color(text, color_code):
+def _change_background_color(text, color_code) -> StyledText:
     """Change the background color of text to the given color code."""
     uncolored_bg = _remove_background_colors(text)
     return _apply_ansi_code(color_code, uncolored_bg)
 
 
-def _remove_background_colors(text):
+def _remove_background_colors(text) -> StyledText:
     """Remove all background coloring from the given text."""
     return _remove_regex(BACKGROUND_COLORS_REGEX, text)
 
 
-def _remove_bold(text):
+def _remove_bold(text) -> StyledText:
     """Remove all text modifications from the given text."""
     return _remove_regex(BOLD_REGEX, text)
 
 
-def _remove_text_colors(text):
+def _remove_text_colors(text) -> StyledText:
     """Remove all foreground coloring from the given text."""
     return _remove_regex(FOREGROUND_COLORS_REGEX, text)
 
 
-def _remove_regex(regex, text):
+def _remove_regex(regex, text) -> StyledText:
     """Remove the given regex from the text."""
     text = str(text)
     if NO_COLOR:
@@ -201,7 +201,7 @@ def _remove_regex(regex, text):
     return StyledText(re.sub(regex, "", text))
 
 
-def _remove_underline(text):
+def _remove_underline(text) -> StyledText:
     """Remove underlining from the given text."""
     return _remove_regex(UNDERLINED_REGEX, text)
 
